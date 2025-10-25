2ba441e9bb8bf0c002ece782d1bb83fb9eb4fa1d
Yun <yunwaddyoo01@gmail.com>
2025-10-25 00:49:29 -0700

    sections folder added

--- front-end/my-app/src/App.jsx
+++ front-end/my-app/src/App.jsx
@@ -1,26 +1,10 @@
-import { useState } from 'react'
+import NewGradOnboarding from './sections/NewGradOnboarding.jsx'
 
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+    return (
+        <div className="scroll-pt-20">
+            
+        </div>
+    )
 }
-
 export default App
